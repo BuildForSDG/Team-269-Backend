66ca21632a0e22d2d7628c03317172b0cca236a0
Bright Onapito <onabright@gmail.com>
2020-05-29 14:08:15 +0300

    feat: define one to many relationship

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     * Get responses defined in ResponseOption model
+     */
+    public function responses()
+    {
+        return $this->hasMany(ResponseOption::class);
+    }
 }
