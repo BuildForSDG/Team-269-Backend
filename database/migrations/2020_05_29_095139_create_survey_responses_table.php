a3ff7a7e974c7ca467b2376c7b8bf13dbfcdf0ae
Bright Onapito <onabright@gmail.com>
2020-05-29 12:55:40 +0300

    chore: add survey_response migration

---
+++ database/migrations/2020_05_29_095139_create_survey_responses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSurveyResponsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_responses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('response_id');
+            $table->unsignedBigInteger('question_id');
+            $table->string('response');
+            $table->timestamps();
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_responses');
+    }
+}
